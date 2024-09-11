d894fe73908365f088501d3d5b00db74e95a9738
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-11 16:21:10 +0200

    init task template

---
+++ src/lif_converters/__init__.py
@@ -0,0 +1,9 @@
+"""
+Package description.
+"""
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("lif-converters")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
