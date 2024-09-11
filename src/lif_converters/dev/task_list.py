75b105c3c21e74ea1fbb7a85eebd1a538696cba2
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-11 16:49:24 +0200

    remove template boilerplate

--- src/lif_converters/dev/task_list.py
+++ src/lif_converters/dev/task_list.py
@@ -1,11 +1,11 @@
 """Contains the list of tasks available to fractal."""
 
-from fractal_tasks_core.dev.task_models import ParallelTask
+from fractal_tasks_core.dev.task_models import NonParallelTask
 
 TASK_LIST = [
-    ParallelTask(
-        name="Thresholding Label Task",
-        executable="thresholding_label_task.py",
+    NonParallelTask(
+        name="Lif Converter Task",
+        executable="lif_converter_task.py",
         meta={"cpus_per_task": 1, "mem": 4000},
     ),
 ]
---
+++ tests/test_lif_converter_task.py
@@ -0,0 +1,8 @@
+import shutil
+from pathlib import Path
+
+import pytest
+from devtools import debug
+from fractal_tasks_core.channels import ChannelInputModel
+
+from lif_converters.lif_converter_task import lif_converter_task
