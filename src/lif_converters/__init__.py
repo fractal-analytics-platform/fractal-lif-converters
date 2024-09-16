f23d3874423f5132484a9bd5465f94e11e983c56
lorenzo <lorenzo.cerrone@uzh.ch>
2024-09-16 14:33:05 +0200

    add wrapper to init

--- src/lif_converters/__init__.py
+++ src/lif_converters/__init__.py
@@ -2,7 +2,11 @@
 
 from importlib.metadata import PackageNotFoundError, version
 
+from lif_converters.converter_wrapper import lif_plate_converter, lif_scene_converter
+
 try:
     __version__ = version("lif-converters")
 except PackageNotFoundError:
     __version__ = "uninstalled"
+
+__all__ = ["lif_plate_converter", "lif_scene_converter"]
