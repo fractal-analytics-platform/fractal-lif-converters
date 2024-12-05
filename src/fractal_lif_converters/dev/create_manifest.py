8a844499e802effc790e9c4c22a7ae861e7ce14c
jluethi <joel.luethi@uzh.ch>
2024-12-05 18:04:52 +0100

    Fix package name in create_manifest

--- src/fractal_lif_converters/dev/create_manifest.py
+++ src/fractal_lif_converters/dev/create_manifest.py
@@ -5,6 +5,6 @@ to the package manifest.
 from fractal_tasks_core.dev.create_manifest import create_manifest
 
 if __name__ == "__main__":
-    PACKAGE = "lif_converters"
+    PACKAGE = "fractal_lif_converters"
     AUTHORS = "Lorenzo Cerrone"
     create_manifest(package=PACKAGE, authors=AUTHORS)
