0f12360a3b968220b3609e15b91a28eea5fd40e1
jluethi <joel.luethi@uzh.ch>
2024-12-12 17:38:00 +0100

    Update docs_url & CI

--- src/fractal_lif_converters/dev/create_manifest.py
+++ src/fractal_lif_converters/dev/create_manifest.py
@@ -7,4 +7,8 @@ from fractal_tasks_core.dev.create_manifest import create_manifest
 if __name__ == "__main__":
     PACKAGE = "fractal_lif_converters"
     AUTHORS = "Lorenzo Cerrone"
-    create_manifest(package=PACKAGE, authors=AUTHORS)
+    docs_link = "https://github.com/fractal-analytics-platform/fractal-lif-converters"
+    if docs_link:
+        create_manifest(package=PACKAGE, authors=AUTHORS, docs_link=docs_link)
+    else:
+        create_manifest(package=PACKAGE, authors=AUTHORS)
